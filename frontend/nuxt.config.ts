8f94ac5f231d043985fcf773280795cc41b600c1
Sergey <sergenevsky@gmail.com>
2025-01-13 00:01:24 +0700

    added .env for easier setup process

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  // ssr: true,
+  ssr: true,
   css: ["~/assets/css/tailwind.css"],
   postcss: {
     plugins: {
