1be4e970f0446f30a34b5922fa7b157577731916
Sergey <sergenevsky@gmail.com>
2025-01-15 23:55:40 +0700

    Deploy state fixing. Added PORT env

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
 
   ssr: true,
 
@@ -31,14 +31,4 @@ export default defineNuxtConfig({
     storesDirs: ['./stores/**']
   },
 
-  // imports: {
-  //   dirs: ["stores"],
-  // },
-
-  // pinia: {
-  //   autoImports: [
-  //     "defineStore", // Auto-import `defineStore`
-  //     "storeToRefs", // Auto-import `storeToRefs`
-  //   ],
-  // }
 })
